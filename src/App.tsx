32130060d7861ab874d0e056c64041974bd2f32f
7i7o <mparij@gmail.com>
2023-05-12 17:23:43 -0300

    update to react-components 0.0.4 & othent 1.0.58

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { OthentLogin } from "@othent/react-components";
+
+const API_ID = import.meta.env.VITE_OTHENT_API_ID;
+
 function App() {
   return (
     <>
       <h1>Othent React Components Test</h1>
       <div className="card">
-        <OthentLogin />
+        <OthentLogin apiid={API_ID} />
       </div>
+      {/* <p className="read-the-docs">Your API_ID={API_ID}</p> */}
       <p className="read-the-docs">
         Click on Login with Othent to test the Othent React Components
       </p>
