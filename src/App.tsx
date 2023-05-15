6a3dc27c3545d6bed6f6d259eacd8160d42de766
7i7o <mparij@gmail.com>
2023-05-15 14:15:41 -0300

    update packages and UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "./App.css";
 import { OthentLogin } from "@othent/react-components";
+import othentLogo from "/logo.svg";
 
 const API_ID = import.meta.env.VITE_OTHENT_API_ID;
 
 function App() {
   return (
     <>
-      <h1>Othent React Components Test</h1>
+      <a href="https://othent.io" target="_blank">
+        <img src={othentLogo} className="logo" alt="Vite logo" />
+      </a>
+      {/* <h1>Othent React Components Test</h1> */}
       <div className="card">
         <OthentLogin apiid={API_ID} />
       </div>
       {/* <p className="read-the-docs">Your API_ID={API_ID}</p> */}
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on Login with Othent to test the Othent React Components
-      </p>
+      </p> */}
     </>
   );
 }
